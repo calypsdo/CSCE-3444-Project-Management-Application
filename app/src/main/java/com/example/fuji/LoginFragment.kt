17da1547dc55c296ef7e46458ce4d0248a265102
calypsdo <anthocookie123@gmail.com>
2021-03-15 22:07:01 -0500

    Adds a variety of images, strings, and styles to the project. In addition to that, this commit will implement the UI for the login and register pages of the application

--- app/src/main/java/com/example/fuji/LoginFragment.kt
+++ app/src/main/java/com/example/fuji/LoginFragment.kt
@@ -6,26 +6,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class LoginFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.login_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        view.findViewById<TextView>(R.id.register_text).setOnClickListener {
+            findNavController().navigate(R.id.action_LoginFragment_to_RegisterFragment)
+        }
+
+        view.findViewById<TextView>(R.id.login_button_text).setOnClickListener {
+            val errorMessageTextView = view.findViewById<TextView>(R.id.login_error_message)
+            //will fix the below line later
+            //errorMessageTextView.visibility = 2
         }
     }
 }

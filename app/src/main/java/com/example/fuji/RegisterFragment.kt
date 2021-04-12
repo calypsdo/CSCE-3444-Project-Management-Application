e141acf796b4f1ed4bb37cbb38b72b26f15ede83
Chet Lockwood <Chet.Lockwood@gmail.com>
2021-04-11 21:39:02 -0500

    Got email creation, confirmation email, and login authentication working

--- app/src/main/java/com/example/fuji/RegisterFragment.kt
+++ app/src/main/java/com/example/fuji/RegisterFragment.kt
@@ -1,7 +1,9 @@
 package com.example.fuji
 
+import android.nfc.Tag
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +13,11 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.example.fuji.RegisterFragment.Companion.TAG
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import java.util.*
 
 /**
@@ -18,16 +25,23 @@ import java.util.*
  */
 class RegisterFragment : Fragment() {
 
+    // [START declare auth]
+    private lateinit var auth: FirebaseAuth
+    // [END declare_auth]
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+            savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.register_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        auth = Firebase.auth
 
         val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+".toRegex()
         val emailInput = view.findViewById<EditText>(R.id.register_email_entry).text
@@ -50,14 +64,88 @@ class RegisterFragment : Fragment() {
                 Toast.makeText(context, "Confirmation does not match password", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
+            if (passwordInput.toString().trim().length < 6) {
+                Toast.makeText(context, "Password must be greater than six characters", Toast.LENGTH_SHORT).show()
+            }
             if (!emailInput.toString().matches(emailPattern)) {
                 Toast.makeText(context, "Invalid email address", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            Toast.makeText(context, "Valid email address", Toast.LENGTH_SHORT).show()
-            Handler().postDelayed({findNavController().navigate(R.id.action_RegisterFragment_to_LoginFragment)}, 2000)
+            createAccount(emailInput.toString().trim(), passwordInput.toString().trim())
+            sendEmailVerification()
+            Handler().postDelayed({findNavController().navigate(R.id.action_RegisterFragment_to_LoginFragment)}, 3000)
         }
     }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////START OF FIREBASE///////////////////////////////////////////
+
+    public override fun onStart() {
+        super.onStart()
+
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload()
+        }
+    }
+
+    private fun createAccount(email: String, password: String){
+        Log.d(TAG, "creatAccount:$email")
+
+        // [START create user with email
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(requireActivity()) { task ->
+            if (task.isSuccessful) {
+                // Sign in success, update UI with the signed-in user's information
+                Log.d(TAG, "createUserWithEmail:success")
+                Toast.makeText(context, "Account Created", Toast.LENGTH_SHORT).show()
+                val user = auth.currentUser
+                updateUI(user)
+            }
+            else {
+                // If sign in fails, display a message to the user
+                Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                Toast.makeText(context, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                updateUI(null)
+            }
+        }
+        // [END create user with email
+    }
+
+    private fun sendEmailVerification() {
+        // [START send email verification]
+        val user = auth.currentUser
+        user.sendEmailVerification().addOnCompleteListener(requireActivity()) { task ->
+            // Email Verification sent
+            if(task.isSuccessful) {
+                Toast.makeText(context, "Verification email sent", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.e(TAG, "sendEmailVerification", task.exception)
+                Toast.makeText(context, "Failed to send verification email", Toast.LENGTH_SHORT).show()
+            }
+        }
+        // [END send email verification
+    }
+
+    private fun reload() {
+        auth.currentUser!!.reload().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                updateUI(auth.currentUser)
+                Toast.makeText(context, "Reload successful!", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.e(TAG, "reload", task.exception)
+                Toast.makeText(context, "Failed to reload user", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+    }
+
+    companion object {
+         private const val TAG = "EmailPassword"
+    }
+    ///////////////////////////////////END OF FIREBASE/////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////
 }
 
 //BUTTON/XML item code for actions

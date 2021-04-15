dbed0a513ad4a6ddd64c89a5842d6a7bb4f4c24a
nbb0040 <nilebarfield@my.unt.edu>
2021-04-15 13:01:20 -0500

    UI for prototype of task fragment

---
+++ app/src/main/java/com/example/fuji/Task_fragment.kt
@@ -0,0 +1,12 @@
+package com.example.fuji
+
+import android.app.AppComponentFactory
+import androidx.fragment.app.Fragment
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class Task_fragment : Fragment() {
+    private val db = Firebase.firestore
+
+}

d97e37fc59e36507a8d3c6344c9ea4e7c830d952
Chet Lockwood <chet.lockwood@gmail.com>
2021-04-15 21:12:51 -0500

    Board UI components

---
+++ app/src/main/java/com/example/fuji/BoardUIFragment.kt
@@ -0,0 +1,18 @@
+package com.example.fuji
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class BoardUIFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.board_ui, container, false)
+    }
+}

22e6e894345d94236c40443eb91a0363c20970ed
calypsdo <anthocookie123@gmail.com>
2021-04-18 15:39:56 -0500

    This is mostly just to fix some version control issues

--- app/src/main/java/com/example/fuji/BoardUIActivity.kt
+++ app/src/main/java/com/example/fuji/BoardUIActivity.kt
@@ -6,25 +6,7 @@ import android.os.Bundle
 class BoardsUIActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_boards)
+        setContentView(R.layout.activity_boards_ui)
 
-        val tempfragment = BoardFragment()
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.board_frame, tempfragment)
-        transaction.commit()
-    }
-
-    fun switchToCreateBoardFragment() {
-        val temp2fragment = CreateBoardFragment()
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.board_frame, temp2fragment)
-        transaction.commit()
-    }
-
-    fun switchToBoardFragment() {
-        val boardfragment = BoardFragment()
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.board_frame, boardfragment)
-        transaction.commit()
     }
 }
